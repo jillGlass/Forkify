ea58ec5520aaccc82d5f12c37d1767c0bc38cd31
JillGlass <glass.nz@gmail.com>
2020-06-16 20:15:38 +1200

    add Search module

--- src/js/index.js
+++ src/js/index.js
@@ -3,16 +3,8 @@
 // get - const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
 //axios automatically converts to json
 
-import axios from "axios";
+import Search from './models/Search'
 
-async function getResults(query) {
-    try{const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`)
-    const recipes = res.data.recipes
-    console.log(recipes)}
-    catch(error) {
-        alert(error)
-    }
-    
-}
-
-getResults('pizza');
+const search = new Search('pizza');
+//console.log(search)
+search.getResults()
