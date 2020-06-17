19dc4178aa12cc8606c5c8e617dc638a7fff2ec8
JillGlass <glass.nz@gmail.com>
2020-06-17 21:17:48 +1200

    set word limit to 17 in recipe title

--- src/js/views/SearchView.js
+++ src/js/views/SearchView.js
@@ -10,6 +10,21 @@ export const clearResults = () => {
     elements.searchResList.innerHTML = "";
 }
 
+export const limitRecipeTitle = (title, limit = 17) => {
+    // if title is over 17letters then wrap text. Split into words, use reduce to count the letters in each word (title word = cur). if accumulated letters + current title (cur) length are less than 17 add to newTitle array.
+    if(title.length > limit) {
+        let newTitle = [];
+        title.split(' ').reduce((acc, cur) => {
+            if(acc + cur.length <= limit) {
+                newTitle.push(cur)
+            } return acc + cur.length;
+        }, 0)
+        //return result
+        return `${newTitle.join(' ')}...`
+
+    } return title
+}
+
 export const renderRecipe = (recipe) => {
   const markUp = `<li>
     <a class="results__link" href="#${recipe.recipe_id}">
@@ -17,7 +32,7 @@ export const renderRecipe = (recipe) => {
             <img src="${recipe.image_url}" alt="${recipe.title}">
         </figure>
         <div class="results__data">
-            <h4 class="results__name">${recipe.title}</h4>
+            <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
             <p class="results__author">${recipe.publisher}</p>
         </div>
     </a>
